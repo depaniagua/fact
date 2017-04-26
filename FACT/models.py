c5b1902d7fd837c2f35f3ef0f29635bc2f689934
David Ernesto Paniagua <de.paniagua@uniandes.edu.co>
2017-04-26 13:35:46 -0500

    actualizacion campos null

--- FACT/models.py
+++ FACT/models.py
@@ -5,6 +5,6 @@ from django.db import models
 from django.db import models
 class Orden(models.Model):
     id= models.IntegerField
-    descripcion= models.CharField(max_length=1000)
-    proveedor = models.CharField(max_length=1000)
-    costo= models.IntegerField
+    descripcion= models.CharField(max_length=1000, null=True)
+    proveedor = models.CharField(max_length=1000, null=True)
+    costo= models.IntegerField (null=True)
