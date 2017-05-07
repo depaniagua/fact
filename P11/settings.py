c11aa5cde01f285e5f9bba5568da95e83ebe37dd
David Ernesto Paniagua <de.paniagua@uniandes.edu.co>
2017-05-07 14:22:50 -0500

    cambio de nombre de basedir

--- P11/settings.py
+++ P11/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'P11.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'FACT/templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
