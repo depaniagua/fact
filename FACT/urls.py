b222563668e1f5488e9ddbba464c26161c51d5d1
David Ernesto Paniagua <de.paniagua@uniandes.edu.co>
2017-05-07 13:54:37 -0500

    corrección url2

--- FACT/urls.py
+++ FACT/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^orden$', views.orden, name ='orden'),
+    url(r'^orden/$', views.orden, name ='orden'),
     url(r'^ListarOrdenes/$', views.OrdenLista.as_view(), name='ListarOrdenes'),
     url(r'^crearOrden/$', views.crearOrden, name='crearOrden'),
 ]
